5f7573b6c1b67c1b3ba6de35b7f78baa4ac577e9
AR <rybakovwebdev@gmail.com>
2024-01-12 01:39:38 +0400

    optimize for mobile

--- src/app/page.js
+++ src/app/page.js
@@ -22,23 +22,25 @@ export default function Home() {
         </header>
         <div className={styles.infoWrapper}>
           <p className={styles.description}>Cozy place in Vera Park. Mixed drinks, craft beer & kitchen.</p>
-          <table>
-            <tbody>
-              <tr>
-                <td className={`${styles.day} ${styles.tableCell}`}>MON-THU:</td>
-                <td className={styles.time}>11 am - 11 pm</td>
-              </tr>
-              <tr>
-                <td className={`${styles.day} ${styles.tableCell}`}>FRI-SUN:</td>
-                <td className={styles.time}>14 pm - 2 am</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className={styles.timetableWrapper}>
+            <table>
+              <tbody>
+                <tr>
+                  <td className={`${styles.day} ${styles.tableCell}`}>MON-THU:</td>
+                  <td className={styles.time}>11 am - 11 pm</td>
+                </tr>
+                <tr>
+                  <td className={`${styles.day} ${styles.tableCell}`}>FRI-SUN:</td>
+                  <td className={styles.time}>14 pm - 2 am</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
         <NavMenu handleMenuClick={handleMenuClick} />
         <MenuImage slug={imageSlug} />
-        <Footer />
       </div>
+      <Footer />
     </main>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,6 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      {/* <h3 className={styles.address}>Address:</h3> */}
       <p>Tbilisi, Merab Kostava 35</p>
       <p>Entrance from Kostava and Vera Park</p>
     </footer>
