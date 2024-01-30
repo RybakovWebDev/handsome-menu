6e5c399bdd21a6a03cbf5bd1cfa7804074916f5c
AR <rybakovwebdev@gmail.com>
2024-01-30 18:08:22 +0400

    Remove icon dependency

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,5 +1,5 @@
 import { Instagram, PhoneCall } from "react-feather";
-import { PiTelegramLogoThin } from "react-icons/pi";
+import Telegram from "../../public/images/telegram-logo-thin.svg";
 
 import foodMenu from "../../public/images/food.jpg";
 import cocktailsMenu from "../../public/images/cocktails.jpg";
@@ -7,7 +7,13 @@ import coffeeMenu from "../../public/images/coffee.jpg";
 import spiritsMenu from "../../public/images/spirits.jpg";
 import teaSoftMenu from "../../public/images/teaSoft.jpg";
 import wineBeerMenu from "../../public/images/wineBeer.jpg";
+import Image from "next/image";
 
+const telegramSvg = (
+  <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' fill='#000000' viewBox='0 0 256 256'>
+    <path d='M236.88,26.19a9,9,0,0,0-9.16-1.57L25.06,103.93a14.22,14.22,0,0,0,2.43,27.21L80,141.45V200a15.92,15.92,0,0,0,10,14.83,15.91,15.91,0,0,0,17.51-3.73l25.32-26.26L173,220a15.88,15.88,0,0,0,10.51,4,16.3,16.3,0,0,0,5-.79,15.85,15.85,0,0,0,10.67-11.63L239.77,35A9,9,0,0,0,236.88,26.19Zm-61.14,36L86.15,126.35l-49.6-9.73ZM96,200V152.52l24.79,21.74Zm87.53,8L100.85,135.5l119-85.29Z'></path>
+  </svg>
+);
 export const MENULINKS = [
   {
     title: "Food",
@@ -353,7 +359,7 @@ export const CONTACTS = [
   {
     title: "Telegram",
     link: "https://t.me/Sky_mother",
-    icon: <PiTelegramLogoThin size={30} />,
+    icon: <Image src={Telegram} alt='Telegram icon' height={30} width={30} />,
     description: "Contact us on Telegram",
   },
   {
