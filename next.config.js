8108e1a2eeb43ce950ce464983097c9ddba497b2
AR <rybakovwebdev@gmail.com>
2024-01-30 20:45:08 +0400

    add loader before rendering map

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "maps.googleapis.com", port: "", pathname: "/maps/**" }],
+  },
+};
 
 module.exports = nextConfig;
