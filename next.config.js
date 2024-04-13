cf0643dedc047b37426fdccbc6d512d22ad884b2
AR <rybakovwebdev@gmail.com>
2024-04-13 16:14:38 +0400

    Add Content Security Policy in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    frame-src 'self' https://www.google.com;
+    upgrade-insecure-requests;
+`;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
