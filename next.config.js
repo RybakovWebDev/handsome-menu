33330ada25f20e3658c3525cd3d24df35ed8622c
AR <rybakovwebdev@gmail.com>
2024-04-13 16:29:24 +0400

    Revert "Update CSP to forbid inline scripts and styles"
    This reverts commit 44f8bde2a23e48b56cea13ecc249eb76a3489ed2.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval';
-    style-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
